f9ab22460a4c6a42dccc2816124defd3e468e8d2
Rizz <123063244+Noctophile@users.noreply.github.com>
2024-12-15 05:01:39 -0800

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "21857983"))
 API_HASH = environ.get("API_HASH", "e469e84c943ce3b8b056eb6a296f2c67")
 BOT_TOKEN = environ.get("BOT_TOKEN", "8030607379:AAFfRjpTIUOsF0aNtFQxLDvoTEmJx3JaT7U")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://envs.sh/BcS.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "Alienstream_bot") # without @
 PORT = environ.get("PORT", "8080")
